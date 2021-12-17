3013b46ecaf76fccefef6bb7fcaa773b5c55fc77
mikalai.kryvusha <mikalai.kryvusha@zorachka.com>
2021-12-17 17:04:03 +0300

    - little refactor

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 
 class TestItems:
 
-    def test_items(self, browser):
+    def test_check_basket_button_exists(self, browser):
 
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
@@ -20,11 +20,9 @@ class TestItems:
         assert button
 
     def wait_for_locator(self, browser, locator, timeout):
-
         while True:
             if timeout == 0:
                 raise Exception("Timeout!")
-
             try:
                 element = browser.find_element_by_css_selector(locator)
                 return True
